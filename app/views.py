b25011d817919ab58de9ca7ef531bcd97eda9b2a
MrazTevin <millatevin@gmail.com>
2018-02-03 20:52:49 +0300

    dummy header in pitches file

--- app/views.py
+++ app/views.py
@@ -8,9 +8,9 @@ def index():
     return render_template('index.html', message=message)
 
 
-@app.route('pitches.html')
+@app.route('/pitches.html')
 def pitches():
     '''
     will return pitches page and its details
-    '''
+     '''
     return render_template('pitches.html')
