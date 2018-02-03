701122ae2735551362d82967e849d30d6d0550b3
MrazTevin <millatevin@gmail.com>
2018-02-03 20:31:56 +0300

    new dynamic route called pitches

--- app/views.py
+++ app/views.py
@@ -6,3 +6,11 @@ from app import app
 def index():
     message = 'hello flask'
     return render_template('index.html', message=message)
+
+
+@app.route('pitches.html')
+def pitches():
+    '''
+    will return pitches page and its details
+    '''
+    return render_template('pitches.html')
